0ee3017ed9bccfe6216f2d1b4d48bd7d1aaf51db
LLN <lev.kudryashov1994@gmail.com>
2014-11-17 18:43:50 +0600

    Problem with connecting.

--- com.epam.lk/src/main/java/action/Action.java
+++
@@ -1,9 +0,0 @@
-package action;
-
-import javax.servlet.http.HttpServletRequest;
-import java.net.ConnectException;
-
-public interface Action {
-    String execute(HttpServletRequest request) throws ConnectException;
-}
-
